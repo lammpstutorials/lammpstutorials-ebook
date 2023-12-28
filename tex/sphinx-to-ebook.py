0aa551c67dd8a4e19c25d31f31f4bc4e4b2eac4b
Simon Gravelle <simon.gravelle@live.fr>
2023-12-28 20:38:34 +0100

    improved sphinx to ebbok convertor

--- tex/sphinx-to-ebook.py
+++ tex/sphinx-to-ebook.py
@@ -36,21 +36,17 @@ for level in tutorials.keys():
         FIX = FixDocument(tex_file_name)
         FIX.fix_document()
 
-stop
-
-
-
 #non_tutorials = {"solutions": ["solutions"],
 #                 "before-you-start": ["before-you-start"]}
 
-if os.path.exists(git_path+'/ebook/non-tutorials/') is False:
-    os.mkdir(git_path+'/ebook/non-tutorials/')
+if os.path.exists(git_path+'/tex/converted_files/non-tutorials/') is False:
+    os.mkdir(git_path+'/tex/converted_files/non-tutorials/')
 
 print("before-you-start")
 print("-----------------------------------------")
 
-rst_file_name = git_path+'/docs/sphinx/source/non-tutorials/before-you-start.rst'
-tex_file_name = git_path+'/ebook/non-tutorials/before-you-start.tex'
+rst_file_name = git_path+'/lammpstutorials.github.io/docs/sphinx/source/non-tutorials/before-you-start.rst'
+tex_file_name = git_path+'/tex/converted_files/non-tutorials/before-you-start.tex'
 RST = ReadRST(rst_file_name)
 RST.convert_file()
 assert len(RST.label_positions) == 1, """Careful, more than one label"""
@@ -62,8 +58,8 @@ FIX.fix_document()
 print("solutions")
 print("-----------------------------------------")
 
-rst_file_name = git_path+'/docs/sphinx/source/non-tutorials/solutions.rst'
-tex_file_name = git_path+'/ebook/non-tutorials/solutions.tex'
+rst_file_name = git_path+'/lammpstutorials.github.io/docs/sphinx/source/non-tutorials/solutions.rst'
+tex_file_name = git_path+'/tex/converted_files/non-tutorials/solutions.tex'
 RST = ReadRST(rst_file_name)
 RST.convert_file()
 assert len(RST.label_positions) == 1, """Careful, more than one label"""
