af25fc25b821cec086812e7dd682c88da54a107f
Simon Gravelle <simon.gravelle@live.fr>
2024-02-16 21:26:50 +0100

    added dark mode files

--- compile-ebook.sh
+++ compile-ebook.sh
@@ -8,23 +8,34 @@ git pull
 git submodule update --remote
 
 cd converter/
-python3 sphinx-to-ebook.py
+    python3 sphinx-to-ebook.py
 cd ..
 
 # copy bib file (careful with special caracters)
 #cp lammpstutorials.github.io/docs/sphinx/source/journal-article.bib tex/bibliography.bib
 
-cd tex/
-# compile pdf
-pdflatex lammps-tutorials-ebook.tex
-bibtex lammps-tutorials-ebook
-pdflatex lammps-tutorials-ebook.tex
-pdflatex lammps-tutorials-ebook.tex
-
-# replace cover
-mv lammps-tutorials-ebook.pdf _lammps-tutorials-ebook.pdf 
-pdftk ../logo/first-page.pdf _lammps-tutorials-ebook.pdf cat output lammps-tutorials-ebook.pdf
-rm _lammps-tutorials-ebook.pdf
+cd tex-light/
+    # compile pdf
+    pdflatex lammps-tutorials-ebook.tex
+    bibtex lammps-tutorials-ebook
+    pdflatex lammps-tutorials-ebook.tex
+    pdflatex lammps-tutorials-ebook.tex
+    # replace cover
+    mv lammps-tutorials-ebook.pdf _lammps-tutorials-ebook.pdf 
+    pdftk ../logo/first-page.pdf _lammps-tutorials-ebook.pdf cat output lammps-tutorials-ebook.pdf
+    rm _lammps-tutorials-ebook.pdf
+    cp lammps-tutorials-ebook.pdf ../ebook/lammps-tutorials-ebook-lm.pdf
 cd ..
 
-cp tex/lammps-tutorials-ebook.pdf ebook/lammps-tutorials-ebook-lm.pdf
+cd tex-dark/
+    # compile pdf
+    pdflatex lammps-tutorials-ebook.tex
+    bibtex lammps-tutorials-ebook
+    pdflatex lammps-tutorials-ebook.tex
+    pdflatex lammps-tutorials-ebook.tex
+    # replace cover
+    mv lammps-tutorials-ebook.pdf _lammps-tutorials-ebook.pdf 
+    pdftk ../logo/first-page.pdf _lammps-tutorials-ebook.pdf cat output lammps-tutorials-ebook.pdf
+    rm _lammps-tutorials-ebook.pdf
+    cp lammps-tutorials-ebook.pdf ../ebook/lammps-tutorials-ebook-dm.pdf
+cd ..
--- converter/sphinx-to-ebook.py
+++ converter/sphinx-to-ebook.py
@@ -11,79 +11,81 @@ from tutorialslist import tutorials, non_tutorials
 
 sys.path.append(git_path + "/functions/")
 
-mode="dark"
-
 from ReadRST import ReadRST
 from WriteTEX import WriteTex
 from FixDocument import FixDocument
 
-if os.path.exists(git_path+'/tex/converted_files') is False:
-    os.mkdir(git_path+'/tex/converted_files')
+for mode in ["light", "dark"]:
 
-for level in tutorials.keys():
-    if os.path.exists(git_path+'/tex/converted_files/'+level) is False:
-        os.mkdir(git_path+'/tex/converted_files/'+level)
-    for tutorial in tutorials[level]:
-        rst_path = git_path+'/lammpstutorials.github.io/docs/sphinx/source/tutorials/'+level+'/'
-        rst_file_name = rst_path+tutorial+'.rst'
-        tex_file_name = git_path+'/tex/converted_files/'+level+'/'+tutorial+'.tex'   
-        RST = ReadRST(rst_file_name, rst_path)
-        RST.convert_file()
-        assert len(RST.label_positions) == 1, """Careful, more than one label"""
-        TEX = WriteTex(tex_file_name, RST, git_path, rst_path, mode)
-        TEX.convert_file()
-        FIX = FixDocument(tex_file_name)
-        FIX.fix_document()
+    folder = '/tex-'+mode+'/'
 
-if os.path.exists(git_path+'/tex/converted_files/non-tutorials/') is False:
-    os.mkdir(git_path+'/tex/converted_files/non-tutorials/')
+    if os.path.exists(git_path+folder+'converted_files') is False:
+        os.mkdir(git_path+folder+'converted_files')
 
-for level in non_tutorials.keys():
-    if os.path.exists(git_path+'/tex/converted_files/'+level) is False:
-        os.mkdir(git_path+'/tex/converted_files/'+level)
-    for tutorial in non_tutorials[level]:
-        rst_path = git_path+'/lammpstutorials.github.io/docs/sphinx/source/tutorials/'+level+'/'
-        rst_file_name = rst_path+tutorial+'.rst'
-        tex_file_name = git_path+'/tex/converted_files/'+level+'/'+tutorial+'.tex'   
-        RST = ReadRST(rst_file_name, rst_path)
-        RST.convert_file()
-        assert len(RST.label_positions) == 1, """Careful, more than one label"""
-        TEX = WriteTex(tex_file_name, RST, git_path, rst_path, mode, nonumber=True)
-        TEX.convert_file()
-        FIX = FixDocument(tex_file_name)
-        FIX.fix_document()
+    for level in tutorials.keys():
+        if os.path.exists(git_path+folder+'converted_files/'+level) is False:
+            os.mkdir(git_path+folder+'converted_files/'+level)
+        for tutorial in tutorials[level]:
+            rst_path = git_path+'/lammpstutorials.github.io/docs/sphinx/source/tutorials/'+level+'/'
+            rst_file_name = rst_path+tutorial+'.rst'
+            tex_file_name = git_path+folder+'converted_files/'+level+'/'+tutorial+'.tex'   
+            RST = ReadRST(rst_file_name, rst_path)
+            RST.convert_file()
+            assert len(RST.label_positions) == 1, """Careful, more than one label"""
+            TEX = WriteTex(tex_file_name, RST, git_path, rst_path, mode)
+            TEX.convert_file()
+            FIX = FixDocument(tex_file_name)
+            FIX.fix_document()
 
-rst_path = git_path + '/lammpstutorials.github.io/docs/sphinx/source/non-tutorials/'
-rst_file_name = rst_path + 'before-you-start.rst'
-tex_file_name = git_path+'/tex/converted_files/non-tutorials/before-you-start.tex'
-RST = ReadRST(rst_file_name, rst_path)
-RST.convert_file()
-assert len(RST.label_positions) == 1, """Careful, more than one label"""
-TEX = WriteTex(tex_file_name, RST, git_path, rst_path, mode, nonumber=True)
-TEX.convert_file()
-FIX = FixDocument(tex_file_name)
-FIX.fix_document()
+    if os.path.exists(git_path+folder+'converted_files/non-tutorials/') is False:
+        os.mkdir(git_path+folder+'converted_files/non-tutorials/')
 
-rst_path = git_path + '/lammpstutorials.github.io/docs/sphinx/source/non-tutorials/'
-rst_file_name = rst_path + 'solutions.rst'
-tex_file_name = git_path+'/tex/converted_files/non-tutorials/solutions.tex'
-RST = ReadRST(rst_file_name, rst_path)
-RST.convert_file()
-assert len(RST.label_positions) == 1, """Careful, more than one label"""
-TEX = WriteTex(tex_file_name, RST, git_path, rst_path, mode, nonumber=True)
-TEX.convert_file()
-FIX = FixDocument(tex_file_name)
-FIX.fix_document()
+    for level in non_tutorials.keys():
+        if os.path.exists(git_path+folder+'converted_files/'+level) is False:
+            os.mkdir(git_path+folder+'converted_files/'+level)
+        for tutorial in non_tutorials[level]:
+            rst_path = git_path+'/lammpstutorials.github.io/docs/sphinx/source/tutorials/'+level+'/'
+            rst_file_name = rst_path+tutorial+'.rst'
+            tex_file_name = git_path+folder+'converted_files/'+level+'/'+tutorial+'.tex'   
+            RST = ReadRST(rst_file_name, rst_path)
+            RST.convert_file()
+            assert len(RST.label_positions) == 1, """Careful, more than one label"""
+            TEX = WriteTex(tex_file_name, RST, git_path, rst_path, mode, nonumber=True)
+            TEX.convert_file()
+            FIX = FixDocument(tex_file_name)
+            FIX.fix_document()
 
-rst_path = git_path + '/lammpstutorials.github.io/docs/sphinx/source/non-tutorials/'
-rst_file_name = rst_path + 'glossary.rst'
-tex_file_name = git_path+'/tex/converted_files/non-tutorials/glossary.tex'
-RST = ReadRST(rst_file_name, rst_path)
-RST.convert_file()
-assert len(RST.label_positions) == 1, """Careful, more than one label"""
-TEX = WriteTex(tex_file_name, RST, git_path, rst_path, mode, nonumber=True)
-TEX.convert_file()
-FIX = FixDocument(tex_file_name)
-FIX.fix_document()
+    rst_path = git_path + '/lammpstutorials.github.io/docs/sphinx/source/non-tutorials/'
+    rst_file_name = rst_path + 'before-you-start.rst'
+    tex_file_name = git_path+folder+'converted_files/non-tutorials/before-you-start.tex'
+    RST = ReadRST(rst_file_name, rst_path)
+    RST.convert_file()
+    assert len(RST.label_positions) == 1, """Careful, more than one label"""
+    TEX = WriteTex(tex_file_name, RST, git_path, rst_path, mode, nonumber=True)
+    TEX.convert_file()
+    FIX = FixDocument(tex_file_name)
+    FIX.fix_document()
 
-print("Compilation completed")
+    rst_path = git_path + '/lammpstutorials.github.io/docs/sphinx/source/non-tutorials/'
+    rst_file_name = rst_path + 'solutions.rst'
+    tex_file_name = git_path+folder+'converted_files/non-tutorials/solutions.tex'
+    RST = ReadRST(rst_file_name, rst_path)
+    RST.convert_file()
+    assert len(RST.label_positions) == 1, """Careful, more than one label"""
+    TEX = WriteTex(tex_file_name, RST, git_path, rst_path, mode, nonumber=True)
+    TEX.convert_file()
+    FIX = FixDocument(tex_file_name)
+    FIX.fix_document()
+
+    rst_path = git_path + '/lammpstutorials.github.io/docs/sphinx/source/non-tutorials/'
+    rst_file_name = rst_path + 'glossary.rst'
+    tex_file_name = git_path+folder+'converted_files/non-tutorials/glossary.tex'
+    RST = ReadRST(rst_file_name, rst_path)
+    RST.convert_file()
+    assert len(RST.label_positions) == 1, """Careful, more than one label"""
+    TEX = WriteTex(tex_file_name, RST, git_path, rst_path, mode, nonumber=True)
+    TEX.convert_file()
+    FIX = FixDocument(tex_file_name)
+    FIX.fix_document()
+
+    print("Compilation completed for", mode, "mode")
